a28c82143869aac2d352f6a792dab432454b74bc
Wesley Ellis <tahnok@gmail.com>
2015-12-08 15:42:05 -0500

    day 3

--- day_3/part2.rb
+++ day_3/part2.rb
@@ -1,44 +1,45 @@
 directions = File.read('input.txt').chars.each_slice(2).to_a[0..-2]
 
-real = {
-  x: 0,
-  y: 0,
-}
-
-robo = {
-  x: 0,
-  y: 0,
-}
-
 visited = {}
 
-def move(santa, direction, visited)
-  key = [santa[:x], santa[:y]].join(',')
+x = 0
+y = 0
+
+robo_x = 0
+robo_y = 0
+
+def update_visited(visited, x, y)
+  key = "#{x},#{y}"
   if visited[key]
     visited[key] += 1
   else
     visited[key] = 1
   end
-
-  case direction
-  when "v"
-    santa[:y] -= 1
-  when "^"
-    santa[:y] += 1
-  when ">"
-    santa[:y] += 1
-  when "<"
-    santa[:y] -= 1
-  end
-  [santa, visited]
 end
 
+def update_dir(direction, x, y)
+  case direction
+  when "v"
+    y -= 1
+  when "^"
+    y += 1
+  when ">"
+    x += 1
+  when "<"
+    x -= 1
+  end
+  [x, y]
+end
 
-directions.each do |real_dir, robo_dir|
-  real, visited = move(real, real_dir, visited)
-  robo, visited = move(robo, robo_dir, visited)
+directions.each do |direction, robo_dir|
+  update_visited(visited, x, y)
+  x, y = update_dir(direction, x, y)
+
+  update_visited(visited, robo_x, robo_y)
+  robo_x, robo_y = update_dir(robo_dir, robo_x, robo_y)
+
 end
 
 puts visited.keys.length
-require 'pry'; binding.pry
-"test"
+
+#2651
