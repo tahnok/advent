5fc03970d79cecef578d1fc2b77b62923c77f1a3
Wesley Ellis <tahnok@gmail.com>
2015-12-18 19:57:22 -0500

    day 13

---
+++ day_13/part2.rb
@@ -0,0 +1,35 @@
+require 'pry'
+raw = File.readlines('input2.txt').map(&:chomp)
+
+seating = {}
+
+def uniq_cycles(array)
+  head = array.shift
+
+  array.permutation.map{|tail|
+    [head] + tail
+  }
+end
+
+def net_happiness(seating, order)
+  order.each_cons(3).map{|left, middle, right|
+    seating[middle][left] + seating[middle][right]
+  }.inject(&:+) +
+  seating[order[0]][order[1]] + seating[order[0]][order[-1]] +
+  seating[order[-1]][order[0]] + seating[order[-1]][order[-2]]
+end
+
+raw.each do |arrangement|
+  /(?<target>\w+) would (?<direction>\w+) (?<ammount>\d+) happiness units by sitting next to (?<dest>\w+)/ =~ arrangement
+  seating[target] ||= {}
+  ammount = ammount.to_i
+  ammount = -ammount if direction == "lose"
+  seating[target][dest] = ammount
+end
+
+puts uniq_cycles(seating.keys).map {|order|
+  net_happiness(seating, order)
+}.max
+
+binding.pry
+
