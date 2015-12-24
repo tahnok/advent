61ffd80d83562287f398b35547b13de4c075eb63
Wesley Ellis <tahnok@gmail.com>
2015-12-23 21:17:42 -0500

    day 15

---
+++ day_15/part2.rb
@@ -0,0 +1,23 @@
+require 'pry'
+
+ingredients = Hash[File.open('input.txt').map{|line|
+  /(?<name>\w+): capacity (?<capacity>[\-\d]+), durability (?<durability>[\-\d]+), flavor (?<flavor>[\-\d]+), texture (?<texture>[\-\d+]+), calories (?<calories>[\-\d]+)/ =~ line
+  [name, [
+    capacity.to_i,
+    durability.to_i,
+    flavor.to_i,
+    texture.to_i,
+    calories.to_i,
+  ]]
+}]
+
+puts ingredients.keys.repeated_combination(100).map {|combo|
+  sums = combo.map{|key| ingredients[key]}.transpose.map{|row| row.inject(:+)}
+  if sums.any?{|x| x < 0}
+    0
+  elsif sums[-1] > 500
+    0
+  else
+    sums[0...-1].inject(:*)
+  end
+}.max
