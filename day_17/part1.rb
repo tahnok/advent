a47b763d557bea17b0352791aa612acd0e5abe34
Wesley Ellis <tahnok@gmail.com>
2015-12-24 13:50:15 -0500

    still wip 17

--- day_17/part1.rb
+++ day_17/part1.rb
@@ -1,40 +1,40 @@
 require 'pry';
-# sizes = [
-#   44,
-#   3,
-#   4,
-#   10,
-#   21,
-#   44,
-#   4,
-#   6,
-#   47,
-#   41,
-#   34,
-#   17,
-#   17,
-#   44,
-#   36,
-#   31,
-#   46,
-#   9,
-#   27,
-#   38,
-# ].sort
+$sizes = [
+  44,
+  3,
+  4,
+  10,
+  21,
+  44,
+  4,
+  6,
+  47,
+  41,
+  34,
+  17,
+  17,
+  44,
+  36,
+  31,
+  46,
+  9,
+  27,
+  38,
+].sort.reverse
+
+$eggnog = 150
 
 def combos(eggnog, containers)
-  containers.select{|s| s <= eggnog}.each_with_index.map{|container, index|
-    if container == eggnog
-      [container]
+  # puts "eggnog: #{eggnog} containers: #{containers}"
+  containers.select{|c| eggnog >= c}.each_with_index.map{ |c, index|
+    if c == eggnog
+      [c]
     else
-      combos(eggnog - container, containers[index+1..-1]).map{|thing|
-        thing << container
-      }
+      combos(eggnog - c, containers[index+1..-1]).map {|x| x.push(c)}
     end
-  }.find{|x| x.reduce(:+) == eggnog} || []
+  }.reject{|x| x.empty?}
 end
 
-$sizes = [ 20, 15, 10, 5, 5]
-$eggnog = 25
+#not 53
 
 binding.pry
